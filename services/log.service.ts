4508253113538107b927fa9939128e0c21c9fdb1
vlad.zakharchev <vlad.zakharchev@quadcode.com>
2022-11-26 21:36:41 +0600

    chalk added for editiong text color, start script edited for bash, log service inited

---
+++ services/log.service.ts
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+
+const { bgRed, bgCyan, bgGreen } = chalk;
+
+export const printError = (error: string) => {
+  console.error(`${bgRed(" ERROR ")} - ${error}`);
+};
+
+export const printSuccess = (message: string) => {
+  console.log(`${bgGreen(" SUCCESS ")} - ${message}`);
+};
+
+export const printHelp = () => {
+  const text = "No parameters passed - weather output \n -s city assignment \n -h for help \n -t to save token";
+  console.log(
+    `${bgCyan(
+      " HELP "
+    )}\n ${text}`
+  );
+};
--- weather.ts
+++ weather.ts
@@ -1,10 +1,20 @@
 #!/user/bin/env node
 
 import { getArgs } from "./helpers/args";
+import { printHelp, printSuccess } from "./services/log.service";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
   console.log('args', args);
+  if(args.h) {
+    printHelp();
+  }
+  if(args.s) {
+    //printSuccess();
+  }
+  if(args.t) {
+    
+  }
 }
 
 initCLI();
